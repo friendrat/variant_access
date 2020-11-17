beb1a521cd12e007e7af0ebc8f0851907204cbde
R2D2 <jacob.turner870@gmail.com>
2020-11-17 23:46:35 +0100

    Updated docstrings

--- variant_access_traits/src/lib.rs
+++ variant_access_traits/src/lib.rs
@@ -41,9 +41,24 @@ impl fmt::Display for VariantAccessError {
 
 /// This trait is for querying an enum.
 ///
-/// has_variant is intended to determine if one of the variant fields is of the specified type
+/// has_variant is intended to determine if one of the variant fields is of the specified type.
 /// contains_variants is intended to determine if the active field is of the specified type. If none
 /// of the fields is of specified type, it is intended that an Err be returned.
+///
+/// # Example
+/// ```
+///enum Enum {
+///    F1(i64),
+///    F2(bool)
+///}
+/// let result: bool = has_variant::<i64>(); // assigns true to result
+/// let result: bool = has_variant::<i32>(); // assigns false to result
+///
+/// let instance = Enum::f1(42);
+/// let result = instance.contains_variant::<i64>(); // result has value Ok(true)
+/// let result = instance.contains_variant::<bool>(); // result has value Ok(false)
+/// let result = instance.contains_variant::<i32>(); // result has value Err
+/// ```
 pub trait ContainsVariant {
     fn has_variant<T>(&self) -> bool;
     fn contains_variant<T>(&self) -> Result<bool, VariantAccessError>;
@@ -59,6 +74,20 @@ pub trait ContainsVariant {
 ///
 /// get_variant_mut is similar except it is for return a mutable reference to the raw value of the
 /// active field.
+///
+/// # Example:
+/// ```
+/// enum Enum {
+///     F1(i64),
+///     F2(bool)
+/// }
+/// let instance = Enum::f1(42);
+///
+/// let inner: &i64 = instance.get_variant::<i64>().unwrap(); // assigns &42 to inner_value
+/// // let inner: &bool = instance.get_variant::<bool>().unwrap() // panics because of unhandled Err.
+/// // let inner: &i32 = instance.get_variant::<i32>().unwrap() // will not compile as GetVariant<i32> is not implemented for Enum.
+/// ```
+/// Works similarly for get_variant_mut if instance is mutable; returns mutable references instead.
 pub trait GetVariant<T> {
     fn get_variant(&self) -> Result<&T, VariantAccessError>;
     fn get_variant_mut(&mut self) -> Result<&mut T, VariantAccessError>;
@@ -71,6 +100,34 @@ pub trait GetVariant<T> {
 ///
 /// This method is guaranteed to succeed in the sense that if an value is given whose type does not
 /// match the type of any field, the program will not compile.
+/// # Example:
+/// ```
+/// enum Enum {
+///     F1(i64),
+///     F2(bool)
+/// }
+/// let mut instance = Enum::F1(42);
+///
+/// instance.set_variant(false); // instance now is equal to Enum::F2(false)
+/// // instance.set_variant(""); will not compile as Enum has not field of type &str
+/// ```
+/// This method uses type inference to try and determine which field to use. However this can
+/// be ambiguuous sometimes.
+///
+/// # Example:
+/// ```
+/// enum Enum {
+///     F1(i32),
+///     F2(i64)
+/// }
+///
+/// let mut instance = Enum::F1(42);
+/// instance.set_variante(1); // Is instance equal to Enum::F1(1) or Enum::F2(1) ???
+///
+/// // Do this instead
+/// instance.set_variant(1 as i32); // instance equals Enum::F1(1)
+/// instance.set_variant(1 as i64); // instance equal Enum::F2(1)
+/// ```
 pub trait SetVariant<T> {
     fn set_variant(&mut self, value: T);
 }
